8808614c80e3782afa777d0bb93aef46f326e4b5
Ben Diamond <54002015+DarkCypher777@users.noreply.github.com>
2019-11-15 10:05:51 -0500

    update 2

---
+++ models/article.js
@@ -0,0 +1,20 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema({
+    title: {
+        type: String,
+        require: true
+    },
+    link: {
+        type: String,
+        require: true
+    },
+    note: {
+        type: Schema.Types.ObjectId,
+        ref: "Note"
+    }
+});
+
+var Article = mongoose.model("Article", articleSchema);
+
+module.exports = Article;
--- server.js
+++ server.js
@@ -12,7 +12,9 @@ var app = express();
 
 app.use(logger("dev"));
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+  extended: true
+}));
 app.use(express.json());
 app.use(express.static("public"));
 
@@ -20,17 +22,62 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines
 
 mongoose.connect(MONGODB_URI);
 
-app.get("/scrape", function(req, res) {
-  axios.get("link here").then(function(response) {
+app.get("/scrape", function (req, res) {
+  axios.get("https://old.reddit.com/r/Games/").then(function (response) {
     var $ = cheerio.load(response.data);
 
+    $("https://old.reddit.com/r/Games/").each(function (i, element) {
+      var result = {};
+
+      result.title = $(this)
+        .children("a")
+        .text();
+      result.link = $(this)
+        .children("a")
+        .attr("href");
+
+      db.Article.create(result)
+        .then(function (dbArticle) {
+          console.log(dbArticle);
+        })
+        .catch(function (err) {
+          console.log(err);
+        });
+    });
+
+    res.send("Scrape Complete");
   });
 });
 
+app.get("/articles/:id", function (req, res) {
+  db.Article.findById(req.params.id)
+    .populate("note")
+    .then(function (dbPopulate) {
+      res.json(dbPopulate);
+    });
+});
 
+app.post("/articles/:id", function (req, res) {
+  db.Note.create(req.body)
+    .then(function (dbPopulate) {
+      return db.Article.findOneAndUpdate({
+        _id: req.params.id
+      }, {
+        $push: {
+          note: dbPopulate._id
+        }
+      }, {
+        new: true
+      });
+    })
+    .then(function (dbPopulate) {
+      res.json(dbPopulate);
+    })
+    .catch(function (err) {
+      res.json(err);
+    });
+});
 
-
-
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log("Connect to localhost:" + PORT);
 });
